5dd41acfb16edb293ec9b8dc695630ffea34da85
zh0rchik <georgiy.zyuzin73@mail.ru>
2023-06-06 23:18:07 +0400

    add change record lab6

--- lab6/main.py
+++ lab6/main.py
@@ -26,6 +26,7 @@ class VisitHistory(Model):
 # Создание таблиц в базе данных
 db.create_tables([VisitHistory])
 
+
 # Класс-контроллер CherryPy
 class WebInterface(object):
     @cherrypy.expose
@@ -37,10 +38,11 @@ class WebInterface(object):
         html = '<head><link rel="stylesheet" href="style.css"></head>'
         html += '<body><h1>История посещений поликлиники</h1>'
         html += '<table>'
-        html += '<tr><th>№</th><th>ФИО пациента</th><th>ФИО врача</th><th>Причина обращения</th><th>Длительность</th></tr>'
+        html += '<tr><th>№</th><th>ФИО пациента</th><th>ФИО врача</th><th>Причина обращения</th><th>Длительность</th><th>Действия</th></tr>'
 
         for visit in visits:
-            html += f'<tr><td>{visit.id}</td><td>{visit.patient_name}</td><td>{visit.doctor_name}</td><td>{visit.reason}</td><td>{visit.duration}</td></tr>'
+            html += f'<tr><td>{visit.id}</td><td>{visit.patient_name}</td><td>{visit.doctor_name}</td><td>{visit.reason}</td><td>{visit.duration}</td>'
+            html += f'<td><a href="/edit_visit?id={visit.id}">Изменить</a></td></tr>'
 
         html += '</table>'
 
@@ -67,6 +69,35 @@ class WebInterface(object):
         visit.save()
 
         return 'Посещение успешно добавлено!'
+
+    @cherrypy.expose
+    def edit_visit(self, id):
+        visit = VisitHistory.get(VisitHistory.id == id)
+
+        html = '<head><link rel="stylesheet" href="style.css"></head>'
+        html += '<body>'
+        html += f'<h2>Изменение посещения (ID: {visit.id}):</h2>'
+        html += f'<form method="post" action="update_visit?id={visit.id}">'
+        html += f'<input type="text" name="patient_name" placeholder="ФИО пациента" value="{visit.patient_name}" required><br>'
+        html += f'<input type="text" name="doctor_name" placeholder="ФИО врача" value="{visit.doctor_name}" required><br>'
+        html += f'<textarea name="reason" placeholder="Причина обращения" required>{visit.reason}</textarea><br>'
+        html += f'<input type="number" name="duration" placeholder="Длительность" value="{visit.duration}" required><br>'
+        html += '<input type="submit" value="Обновить">'
+        html += '</form>'
+        html += '</body>'
+
+        return html
+
+    @cherrypy.expose
+    def update_visit(self, id, patient_name, doctor_name, reason, duration):
+        visit = VisitHistory.get(VisitHistory.id == id)
+        visit.patient_name = patient_name
+        visit.doctor_name = doctor_name
+        visit.reason = reason
+        visit.duration = duration
+        visit.save()
+
+        return f'Посещение (ID: {visit.id}) успешно обновлено!'
 
 
 # Конфигурация CherryPy
